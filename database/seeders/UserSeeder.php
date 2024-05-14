dabcd5d4825b1d2fe1510dd60dfebe95c83c8a80
joselomanuelo <joselomanuelo@gmail.com>
2024-05-14 13:42:10 -0500

    feat: user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::factory()->create(['email' => 'admin@admin.com']);
+        User::factory(100)->create();
+    }
+}

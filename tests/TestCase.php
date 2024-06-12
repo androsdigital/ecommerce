eb364c9bdcc34be21050e43f4c44844be6eb4391
joselomanuelo <joselomanuelo@gmail.com>
2024-06-12 10:41:51 -0500

    feat: add useful entities to the test case class.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,10 @@
 
 namespace Tests;
 
+use App\Models\Address;
+use App\Models\City;
+use App\Models\Customer;
+use App\Models\State;
 use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
@@ -9,9 +13,20 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
+    protected State $state;
+
+    protected City $city;
+
+    protected Customer $customer;
+
     protected function setUp(): void
     {
         parent::setUp();
+
+        $this->state = State::factory()->create();
+        $this->city = City::factory()->create();
+        $this->customer = Customer::factory()->create();
+        $this->customer->addresses()->save(Address::factory()->create());
 
         $this->actingAs(User::factory()->create());
     }

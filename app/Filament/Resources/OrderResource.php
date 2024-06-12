a9b1fe2cfbf08cc573f6be0619bcb313bb88dc8d
joselomanuelo <joselomanuelo@gmail.com>
2024-06-12 10:44:34 -0500

    feat: implements list orders resource, including tests.

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -2,14 +2,39 @@
 
 namespace App\Filament\Resources;
 
+use App\Enums\OrderStatus;
+use App\Filament\Resources\OrderResource\Pages\CreateOrder;
+use App\Filament\Resources\OrderResource\Pages\EditOrder;
 use App\Filament\Resources\OrderResource\Pages\ListOrders;
+use App\Filament\Resources\OrderResource\RelationManagers\PaymentsRelationManager;
+use App\Filament\Resources\OrderResource\Widgets\OrderStats;
 use App\Models\Order;
+use App\Models\StockItem;
+use Exception;
+use Filament\Forms\Components\Actions\Action;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Placeholder;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\ToggleButtons;
+use Filament\Forms\Form;
+use Filament\Forms\Set;
 use Filament\Resources\Resource;
-use Filament\Tables\Actions\BulkActionGroup;
 use Filament\Tables\Actions\DeleteBulkAction;
 use Filament\Tables\Columns\Summarizers\Sum;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\TrashedFilter;
+use Filament\Tables\Grouping\Group as TableGroup;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Carbon;
 
 class OrderResource extends Resource
 {
@@ -22,33 +47,226 @@ class OrderResource extends Resource
     public static function table(Table $table): Table
     {
         return $table
-            ->columns([
-                TextColumn::make('product.name')
-                    ->label('Producto'),
-                TextColumn::make('price')
-                    ->label('Precio')
-                    ->summarize(Sum::make()->money())
-                    ->money(),
-                TextColumn::make('created_at')
-                    ->label('Fecha de creación')
-                    ->dateTime(),
-            ])
+            ->columns(self::getTableColumns())
+            ->filters(self::getTableFilters())
             ->bulkActions([
-                BulkActionGroup::make([
-                    DeleteBulkAction::make(),
-                ]),
+                DeleteBulkAction::make(),
             ])
-            ->defaultSort('id', 'desc');
+            ->groups([
+                TableGroup::make('created_at')
+                    ->label('Fecha de creación')
+                    ->date()
+                    ->collapsible(),
+                TableGroup::make('customer.name')
+                    ->label('Comprador')
+                    ->collapsible(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //PaymentsRelationManager::class,
+        ];
+    }
+
+    public static function getWidgets(): array
+    {
+        return [
+            //OrderStats::class,
+        ];
     }
 
     public static function getPages(): array
     {
         return [
-            'index' => ListOrders::route('/'),
+            'index'  => ListOrders::route('/'),
+            'create' => CreateOrder::route('/create'),
+            'edit'   => EditOrder::route('/{record}/edit'),
         ];
     }
 
-    public static function canCreate(): bool
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()->withoutGlobalScope(SoftDeletingScope::class);
+    }
+
+    public static function getGloballySearchableAttributes(): array
+    {
+        return ['number', 'customer.name'];
+    }
+
+    public static function getGlobalSearchResultDetails(Model $record): array
+    {
+        /** @var Order $record */
+
+        return [
+            'Customer' => optional($record->customer)->name,
+        ];
+    }
+
+    public static function getGlobalSearchEloquentQuery(): Builder
+    {
+        return parent::getGlobalSearchEloquentQuery()->with(['customer', 'orderItems']);
+    }
+
+    public static function getNavigationBadge(): ?string
+    {
+        /** @var class-string<Model> $modelClass */
+        $modelClass = static::$model;
+
+        return (string) $modelClass::where('status', 'new')->count();
+    }
+
+    public static function getDetailsFormSchema(): array
+    {
+        return [
+            TextInput::make('number')
+                ->label('Número')
+                ->default('OR-' . random_int(100000, 999999))
+                ->disabled()
+                ->dehydrated()
+                ->required()
+                ->maxLength(32)
+                ->unique(Order::class, 'number', ignoreRecord: true),
+
+            Select::make('customer_id')
+                ->label('Comprador')
+                ->relationship('customer', 'name')
+                ->searchable()
+                ->required(),
+
+            ToggleButtons::make('status')
+                ->inline()
+                ->options(OrderStatus::class)
+                ->required(),
+
+            MarkdownEditor::make('notes')
+                ->label('Observaciones')
+                ->columnSpan('full'),
+        ];
+    }
+
+    public static function getItemsRepeater(): Repeater
+    {
+        return Repeater::make('orderItems')
+            ->relationship()
+            ->schema([
+                Select::make('stock_item_id')
+                    ->relationship('stockItem', 'product.name')
+                    ->label('Producto')
+                    ->required()
+                    ->reactive()
+                    ->afterStateUpdated(fn ($state, Set $set) => $set('unit_price', StockItem::find($state)?->unit_price ?? 0))
+                    ->distinct()
+                    ->disableOptionsWhenSelectedInSiblingRepeaterItems()
+                    ->columnSpan([
+                        'md' => 5,
+                    ])
+                    ->searchable(),
+
+                TextInput::make('quantity')
+                    ->label('Quantity')
+                    ->numeric()
+                    ->default(1)
+                    ->columnSpan([
+                        'md' => 2,
+                    ])
+                    ->required(),
+
+                TextInput::make('unit_price')
+                    ->label('Unit Price')
+                    ->disabled()
+                    ->dehydrated()
+                    ->numeric()
+                    ->required()
+                    ->columnSpan([
+                        'md' => 3,
+                    ]),
+            ])
+            ->defaultItems(1)
+            ->hiddenLabel()
+            ->columns([
+                'md' => 10,
+            ])
+            ->required();
+    }
+
+    public static function getTableColumns(): array
+    {
+        return [
+            TextColumn::make('customer.name')
+                ->label('Comprador')
+                ->toggleable()
+                ->searchable()
+                ->sortable(),
+
+            TextColumn::make('number')
+                ->label('Número')
+                ->toggleable()
+                ->searchable()
+                ->sortable(),
+
+            TextColumn::make('status')
+                ->label('Estado')
+                ->toggleable()
+                ->badge(),
+
+            TextColumn::make('total_price')
+                ->label('Precio total')
+                ->toggleable()
+                ->numeric(locale: 'es')
+                ->sortable()
+                ->summarize(Sum::make('sum')->label('Total')->numeric(locale: 'es')),
+
+            TextColumn::make('address.full_address')
+                ->label('Dirección de entrega')
+                ->toggleable()
+                ->searchable(),
+
+            TextColumn::make('total_price_before_discount')
+                ->label('Precio total sin descuento')
+                ->toggleable(isToggledHiddenByDefault: true)
+                ->numeric(locale: 'es')
+                ->sortable()
+                ->summarize(Sum::make('sum')->label('Total')->numeric(locale: 'es')),
+
+            TextColumn::make('total_discount')
+                ->label('Descuento total')
+                ->toggleable(isToggledHiddenByDefault: true)
+                ->numeric(locale: 'es')
+                ->sortable()
+                ->summarize(Sum::make('sum')->label('Total')->numeric(locale: 'es')),
+
+            TextColumn::make('total_shipping_price')
+                ->label('Costo total de envío')
+                ->toggleable(isToggledHiddenByDefault: true)
+                ->numeric(locale: 'es')
+                ->sortable()
+                ->summarize(Sum::make('sum')->label('Total')->numeric(locale: 'es')),
+
+            TextColumn::make('total_quantity')
+                ->label('Cantidad de productos')
+                ->toggleable(isToggledHiddenByDefault: true)
+                ->numeric(locale: 'es')
+                ->sortable()
+                ->summarize(Sum::make('sum')->label('Total')->numeric(locale: 'es')),
+
+            TextColumn::make('created_at')
+                ->label('Creado el')
+                ->toggleable(isToggledHiddenByDefault: true)
+                ->date()
+                ->sortable(),
+
+            TextColumn::make('updated_at')
+                ->label('Actualizado el')
+                ->toggleable(isToggledHiddenByDefault: true)
+                ->date()
+                ->sortable(),
+        ];
+    }
+
+    public static function getTableFilters(): array
     {
         return false;
     }
---
+++ tests/Feature/OrderResource/ListOrderResourceTest.php
@@ -0,0 +1,146 @@
+<?php
+
+use App\Filament\Resources\OrderResource;
+use App\Filament\Resources\OrderResource\Pages\ListOrders;
+use App\Models\Order;
+use Illuminate\Database\Eloquent\Builder;
+
+use function Pest\Livewire\livewire;
+
+it('can render list page', function () {
+    $this->get(OrderResource::getUrl())->assertSuccessful();
+
+    $this->assertAuthenticated();
+});
+
+it('can list orders', function () {
+    $orders = Order::factory()->count(10)->create();
+    livewire(ListOrders::class)
+        ->assertCanSeeTableRecords($orders)
+        ->assertCountTableRecords(10)
+        ->assertCanRenderTableColumn('customer.name')
+        ->assertCanRenderTableColumn('number')
+        ->assertCanRenderTableColumn('status')
+        ->assertCanRenderTableColumn('total_price')
+        ->assertCanRenderTableColumn('address.full_address')
+        ->assertCanNotRenderTableColumn('total_price_before_discount')
+        ->assertCanNotRenderTableColumn('total_discount')
+        ->assertCanNotRenderTableColumn('total_shipping_price')
+        ->assertCanNotRenderTableColumn('total_quantity')
+        ->assertCanNotRenderTableColumn('created_at')
+        ->assertCanNotRenderTableColumn('updated_at');
+
+    $this->assertAuthenticated();
+});
+
+it('can set correct record values', function () {
+    $orders = Order::factory()->count(10)->create();
+
+    $order = $orders->random();
+
+    livewire(ListOrders::class)
+        ->assertTableColumnStateSet('customer.name', $order->customer->name, record: $order)
+        ->assertTableColumnStateSet('number', $order->number, record: $order)
+        ->assertTableColumnFormattedStateSet('status', $order->status->getLabel(), record: $order)
+        ->assertTableColumnStateSet('total_price', $order->total_price, record: $order)
+        ->assertTableColumnStateSet('address.full_address', $order->address->full_address, record: $order)
+        ->assertTableColumnStateSet('total_price_before_discount', $order->total_price_before_discount, record: $order)
+        ->assertTableColumnStateSet('total_discount', $order->total_discount, record: $order)
+        ->assertTableColumnStateSet('total_shipping_price', $order->total_shipping_price, record: $order)
+        ->assertTableColumnStateSet('total_quantity', $order->total_quantity, record: $order)
+        ->assertTableColumnStateSet('created_at', $order->created_at, record: $order)
+        ->assertTableColumnStateSet('updated_at', $order->updated_at, record: $order);
+});
+
+it('can search orders', function () {
+    $orders = Order::factory()->count(10)->create();
+    $order = $orders->random();
+
+    livewire(ListOrders::class)
+        ->assertCanSeeTableRecords($orders)
+        ->searchTable($order->number)
+        ->assertCanSeeTableRecords($orders->where('number', $order->number))
+        ->assertCountTableRecords($orders->where('number', $order->number)->count())
+        ->searchTable($order->address->full_address)
+        ->assertCanSeeTableRecords(Order::whereHas('address', function (Builder $query) use ($order) {
+            $query->where('full_address', $order->address->full_address);
+        })->get())
+        ->assertCountTableRecords(Order::whereHas('address', function (Builder $query) use ($order) {
+            $query->where('full_address', $order->address->full_address);
+        })->get()->count())
+        ->searchTable($order->customer->name)
+        ->assertCanSeeTableRecords($orders->where('customer_id', $order->customer->id))
+        ->assertCountTableRecords($orders->where('customer_id', $order->customer->id)->count());
+
+    $this->assertAuthenticated();
+});
+
+it('can sort orders by title', function () {
+    $orders = Order::factory()->count(10)->create();
+
+    livewire(ListOrders::class)
+        ->sortTable('costumer.name')
+        ->assertCanSeeTableRecords($orders->sortBy('costumer.name'), inOrder: true)
+        ->sortTable('costumer.name', 'desc')
+        ->assertCanSeeTableRecords($orders->sortByDesc('costumer.name'), inOrder: true)
+        ->sortTable('number')
+        ->assertCanSeeTableRecords($orders->sortBy('number'), inOrder: true)
+        ->sortTable('number', 'desc')
+        ->assertCanSeeTableRecords($orders->sortByDesc('number'), inOrder: true)
+        ->sortTable('total_price_before_discount')
+        ->assertCanSeeTableRecords($orders->sortBy('total_price_before_discount'), inOrder: true)
+        ->sortTable('total_price_before_discount', 'desc')
+        ->assertCanSeeTableRecords($orders->sortByDesc('total_price_before_discount'), inOrder: true)
+        ->sortTable('total_discount')
+        ->assertCanSeeTableRecords($orders->sortBy('total_discount'), inOrder: true)
+        ->sortTable('total_discount', 'desc')
+        ->assertCanSeeTableRecords($orders->sortByDesc('total_discount'), inOrder: true)
+        ->sortTable('total_shipping_price')
+        ->assertCanSeeTableRecords($orders->sortBy('total_shipping_price'), inOrder: true)
+        ->sortTable('total_shipping_price', 'desc')
+        ->assertCanSeeTableRecords($orders->sortByDesc('total_shipping_price'), inOrder: true)
+        ->sortTable('total_quantity')
+        ->assertCanSeeTableRecords($orders->sortBy('total_quantity'), inOrder: true)
+        ->sortTable('total_quantity', 'desc')
+        ->assertCanSeeTableRecords($orders->sortByDesc('total_quantity'), inOrder: true)
+        ->sortTable('created_at')
+        ->assertCanSeeTableRecords($orders->sortBy('created_at'), inOrder: true)
+        ->sortTable('created_at', 'desc')
+        ->assertCanSeeTableRecords($orders->sortByDesc('created_at'), inOrder: true)
+        ->sortTable('updated_at')
+        ->assertCanSeeTableRecords($orders->sortBy('updated_at'), inOrder: true)
+        ->sortTable('updated_at', 'desc')
+        ->assertCanSeeTableRecords($orders->sortByDesc('updated_at'), inOrder: true);
+});
+
+it('can filter by creation date', function () {
+    $orders = Order::factory()->count(10)->create();
+    $ordersFromMonthAgo = $orders->where('created_at', '>=', now()->subMonth());
+    $ordersUntilMonthAgo = $orders->where('created_at', '<', now()->subMonth());
+
+    livewire(ListOrders::class)
+        ->assertCanSeeTableRecords($orders)
+        ->assertCountTableRecords(10)
+        ->filterTable('created_at', [
+            'created_from' => now()->subMonth(),
+        ])
+        ->assertCanSeeTableRecords($ordersFromMonthAgo)
+        ->assertCountTableRecords($ordersFromMonthAgo->count())
+        ->removeTableFilter('created_at')
+        ->filterTable('created_at', [
+            'created_until' => now()->subMonth(),
+        ])
+        ->assertCanSeeTableRecords($ordersUntilMonthAgo)
+        ->assertCountTableRecords($ordersUntilMonthAgo->count());
+});
+
+it('can sum values in a column', function () {
+    $orders = Order::factory()->count(10)->create();
+
+    livewire(ListOrders::class)
+        ->assertCanSeeTableRecords($orders)
+        ->assertTableColumnSummarySet('total_price_before_discount', 'sum', $orders->sum('total_price_before_discount'))
+        ->assertTableColumnSummarySet('total_discount', 'sum', $orders->sum('total_discount'))
+        ->assertTableColumnSummarySet('total_shipping_price', 'sum', $orders->sum('total_shipping_price'))
+        ->assertTableColumnSummarySet('total_quantity', 'sum', $orders->sum('total_quantity'));
+});

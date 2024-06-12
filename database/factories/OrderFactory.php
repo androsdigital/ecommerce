3edb42907c8457d9cfa5c704731836660d5ecacc
joselomanuelo <joselomanuelo@gmail.com>
2024-06-12 09:02:34 -0500

    refactor: modify order factory field values for data consistency.

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -17,19 +17,21 @@ class OrderFactory extends Factory
         $customer = Customer::inRandomOrder()->first();
         $address = $customer->addresses()->inRandomOrder()->first();
 
+        $total_price_before_discount = fake()->randomNumber(5);
+
         return [
             'customer_id' => $customer->id,
             'address_id'  => $address->id,
 
             'number'                      => 'OR' . fake()->unique()->randomNumber(6),
-            'total_price_before_discount' => fake()->randomNumber(5),
-            'total_discount'              => fake()->randomNumber(3),
+            'total_price_before_discount' => $total_price_before_discount,
+            'total_discount'              => intdiv($total_price_before_discount, 2),
             'total_shipping_price'        => fake()->randomNumber(3),
             'total_quantity'              => fake()->randomNumber(1),
             'status'                      => fake()->randomElement(OrderStatus::values()),
             'notes'                       => json_encode(fake()->realText(100)),
-            'created_at'                  => fake()->dateTimeBetween('-1 year'),
-            'updated_at'                  => fake()->dateTimeBetween('-5 month'),
+            'created_at'                  => fake()->dateTimeBetween('-2 month'),
+            'updated_at'                  => fake()->dateTimeBetween('-1 month'),
         ];
     }
 }

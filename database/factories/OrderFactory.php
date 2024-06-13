15c08d4f0e85e7d803f3d929bc5ee3ab830ea0e5
joselomanuelo <joselomanuelo@gmail.com>
2024-06-13 08:24:22 -0500

    refactor: use faker property indeed fake() helper, and add additional discount fields

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -16,21 +16,24 @@ class OrderFactory extends Factory
     {
         $customer = Customer::inRandomOrder()->first();
         $address = $customer->addresses()->inRandomOrder()->first();
+        $discount = $this->faker->randomNumber(2);
 
         return [
             'customer_id' => $customer->id,
             'address_id'  => $address->id,
 
-            'number'                      => 'OR' . fake()->unique()->randomNumber(6),
+            'number'                      => $this->faker->unique()->numerify('OR-######'),
+            'total_price'                 => 0,
             'total_price_before_discount' => 0,
             'total_items_discount'        => 0,
-            'discount'                    => $this->faker->randomNumber(2),
+            'discount'                    => $discount,
+            'total_discount'              => $discount,
             'total_shipping_price'        => 0,
             'total_quantity'              => 0,
-            'status'                      => fake()->randomElement(OrderStatus::values()),
-            'notes'                       => json_encode(fake()->realText(100)),
-            'created_at'                  => fake()->dateTimeBetween('-2 month'),
-            'updated_at'                  => fake()->dateTimeBetween('-1 month'),
+            'status'                      => $this->faker->randomElement(OrderStatus::values()),
+            'notes'                       => json_encode($this->faker->realText(100)),
+            'created_at'                  => $this->faker->dateTimeBetween('-2 month'),
+            'updated_at'                  => $this->faker->dateTimeBetween('-1 month'),
         ];
     }
 }

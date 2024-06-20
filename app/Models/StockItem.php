950e03cb31ceaab800de439e7d3e37a1c53d7ee7
joselomanuelo <joselomanuelo@gmail.com>
2024-06-20 09:44:19 -0500

    refactor: stock item as model not pivot

--- app/Models/StockItem.php
+++ app/Models/StockItem.php
@@ -3,12 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\Pivot;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
-class StockItem extends Pivot implements HasMedia
+class StockItem extends Model implements HasMedia
 {
     use HasFactory;
     use InteractsWithMedia;

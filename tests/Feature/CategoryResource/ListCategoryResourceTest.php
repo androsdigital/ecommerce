4733a4422da54303b22c58e57694f9e0e4890a7f
joselomanuelo <joselomanuelo@gmail.com>
2024-07-22 18:52:12 -0500

    test: improve list resource tests

--- tests/Feature/CategoryResource/ListCategoryResourceTest.php
+++ tests/Feature/CategoryResource/ListCategoryResourceTest.php
@@ -21,10 +21,51 @@ it('can list products', function () {
         ->assertCanRenderTableColumn('name')
         ->assertCanRenderTableColumn('slug')
         ->assertCanNotRenderTableColumn('created_at')
-        ->assertCanNotRenderTableColumn('updated_at')
-        ->searchTable($categories->first()->name)
-        ->assertCanSeeTableRecords($categories->where('name', $categories->first()->name))
-        ->assertCountTableRecords($categories->where('name', $categories->first()->name)->count());
+        ->assertCanNotRenderTableColumn('updated_at');
 
     $this->assertAuthenticated();
+});
+
+it('can set correct record values', function () {
+    $categories = Category::factory(10)->create();
+
+    $category = $categories->random();
+
+    livewire(ListCategories::class)
+        ->assertTableColumnStateSet('name', $category->name, record: $category)
+        ->assertTableColumnStateSet('slug', $category->slug, record: $category)
+        ->assertTableColumnStateSet('created_at', $category->created_at, record: $category)
+        ->assertTableColumnStateSet('updated_at', $category->updated_at, record: $category);
+});
+
+it('can search categories', function () {
+    $categories = Category::factory(10)->create();
+
+    $category = $categories->random();
+
+    livewire(ListCategories::class)
+        ->assertCanSeeTableRecords($categories)
+        ->searchTable($category->name)
+        ->assertCanSeeTableRecords($categories->where('name', $category->name))
+        ->assertCountTableRecords($categories->where('name', $category->name)->count());
+
+    $this->assertAuthenticated();
+});
+
+it('can sort addresses', function () {
+    $categories = Category::factory(10)->create();
+
+    livewire(ListCategories::class)
+        ->sortTable('name')
+        ->assertCanSeeTableRecords($categories->sortBy('name'), inOrder: true)
+        ->sortTable('name', 'desc')
+        ->assertCanSeeTableRecords($categories->sortByDesc('name'), inOrder: true)
+        ->sortTable('created_at')
+        ->assertCanSeeTableRecords($categories->sortBy('created_at'), inOrder: true)
+        ->sortTable('created_at', 'desc')
+        ->assertCanSeeTableRecords($categories->sortByDesc('created_at'), inOrder: true)
+        ->sortTable('updated_at')
+        ->assertCanSeeTableRecords($categories->sortBy('updated_at'), inOrder: true)
+        ->sortTable('updated_at', 'desc')
+        ->assertCanSeeTableRecords($categories->sortByDesc('updated_at'), inOrder: true);
 });
